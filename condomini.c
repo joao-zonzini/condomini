d98b2303d5a15900f2a90dad6db573f3ed183de8
Joao Zonzini <zonzinijoao@gmail.com>
2025-04-21 18:49:41 -0300

    adicionei comentários mais completos

--- condomini.c
+++ condomini.c
@@ -1,25 +1,32 @@
-// condomini.c 
-// simulação da questão 5 da 40a Olimpíada de Matemática da Unicamp
-// jaz 21/04/25
+// Programa: condomini.c 
+// Objetivo: simular a questão 5 da 40a Olimpíada de Matemática da Unicamp
+// Autor: João Pedro de Azevedo Zonzini @ 21/04/25
 
+// importação das bibliotecas padrões de C
 #include <stdlib.h>
 #include <stdio.h>
 
-#define LINHAS 3    // Número de linhas da matriz
-#define COLUNAS 3   // Número de colunas da matriz
+// generalização do tamanho do condominio
+#define LINHAS 3    // linhas da matriz que representa o condominio
+#define COLUNAS LINHAS
+// o condominio é quadrado
+// o numero de casas no condominio é LINHAS * COLUNAS
 
+//declaração das funções
 void printar_condo(int matriz[LINHAS][COLUNAS]); void inserir_perrengues(int matriz[LINHAS][COLUNAS]);
 int condo_deboa(int matriz[LINHAS][COLUNAS]); void troca_perrengues(int matriz[LINHAS][COLUNAS]);
 
 int main(void){
-	
-	// inicializando a matriz condominio com zeros
+	// inicialização da matriz condominio com zeros perrengues
 	int condo[LINHAS][COLUNAS] = {0};
-	int noite = 0;
-	int deboa = 0;
+
+	// inicialização das variaveis noite e deboa
+	int noite = 0; // conta o # de noites
+	int deboa = 0; // variavel de estado do condominio
 
 	puts("Bem-vindo ao Condominio Planolandia! No Condomínio cada casa tem um número:");
 
+	// imprimir na tela os numeros das casas
 	for(int i = 0; i < LINHAS; i++) {
 		for(int j = 0; j < COLUNAS; j++) {
 			printf("\t%d%d ", i+1, j+1);
@@ -33,26 +40,34 @@ int main(void){
 
 	printf("Dê perrengues para cada casa. Lembre-se que o número de perrengues deve ser positivo.\n");
 	
+	// chama função que recebe o numero de perrengues em cada casa
 	inserir_perrengues(condo);
 
+	// mostra de volta os perrengues para o usuario
 	printf("Os perrengues ficaram assim:\n\n");
 	printar_condo(condo);
 	puts("");
 
+	// analiza o estado do condominio antes da trocação de perrengues
 	if (condo_deboa(condo)) {
-		printf("O Condominio está de boa.\n");
+		printf("O Condominio está de boa.\n"); // não precisa fazer a troca de perrengues
 	} else {
 		printf("O Condomínio está em pé de guerra! Vamos ver a troca de perrengues:\n");
 	}
 
+	// executa código enquanto o condominio não fica de boa
 	while(!condo_deboa(condo)) {
+		// simula uma noite --> adiciona no contador
 		noite++;
 		printf("----------- Noite #%d -----------\n", noite);
+		
+		// executa a troca de perrengues
 		troca_perrengues(condo);
 
+		// imprime a nova configuraçao do condominio
 		printar_condo(condo);
-	}
-
+	}	// saiu do loop --> condominio ficou de boa
+ 
 	if (noite > 0) {
 		puts("-----------> de boa <-----------");
 		printf("Foram necessárias %d noites para o condomínio ficar de boa.\n", noite);
@@ -62,6 +77,7 @@ int main(void){
 }
 
 void printar_condo(int matriz[LINHAS][COLUNAS]){
+	// para imprimir a configuração do condominio, é preciso percorrer cada posição da matriz
 	for(int i = 0; i < LINHAS; i++) {
 		for(int j = 0; j < COLUNAS; j++) {
 			printf("\t%d ", matriz[i][j]);
@@ -71,11 +87,17 @@ void printar_condo(int matriz[LINHAS][COLUNAS]){
 }
 
 void inserir_perrengues(int matriz[LINHAS][COLUNAS]) {
+	// inicialização de variavel de input do usuario
 	int input;
+
+	// para cada casa do condominio, inserir um número de perrengues
 	for (int i = 0; i < LINHAS; i++) {
 		for (int j = 0; j < COLUNAS; j++) {
 			while (1) {
+				// se livrando de possível lixo na variavel input
 				input = 0;
+
+				// recebendo do usuario
 				printf("Dê perrengues para a casa %d%d: ", i+1, j+1);
 				scanf("%d", &input);
 
@@ -92,21 +114,23 @@ void inserir_perrengues(int matriz[LINHAS][COLUNAS]) {
 }
 
 int condo_deboa(int matriz[LINHAS][COLUNAS]) {
+	// verificar se o condominio está de boa significa se verificar se existe alguma casa com 4 perrengues ou mais
 	for(int i = 0; i < LINHAS; i++) {
 		for(int j = 0; j < COLUNAS; j++) {
 			if(matriz[i][j] >= 4) {
-				return 0;
+				return 0; // zero significa que o condominio não está de boa
 			}
 		}
 	}
-
-	return 1;
+	return 1; // um significa que o condominio está de boa
 }
 
 void troca_perrengues(int matriz[LINHAS][COLUNAS]) {
+	// inicialização de uma matriz temporária que vai registrar qual a diferença de perrengues
+	// entre a configuração atual e a configuração nova do condominio
 	int matriz_despejo[LINHAS][COLUNAS] = {0};
 
-	// Matriz como os deslocamentos para os vizinhos diretamente adjacentes
+	// matriz como os deslocamentos para os vizinhos diretamente adjacentes
     int deslocamentos[4][2] = {
         {-1, 0}, // cima
         {1, 0},  // baixo
@@ -116,14 +140,17 @@ void troca_perrengues(int matriz[LINHAS][COLUNAS]) {
 
 	for (int i = 0; i < LINHAS; i++) {
 		for (int j = 0; j < COLUNAS; j++) {
-			if (matriz[i][j] >= 4) { // se a casa tem perrengues, ela despeja 4
+			if (matriz[i][j] >= 4) { // se a casa tem perrengues, ela sempre despeja 4
 				matriz_despejo[i][j] = matriz_despejo[i][j] - 4;
-				for (int k = 0; k < 4; k++) { // joga os perrengues nos vizinhos adjacentes
+				for (int k = 0; k < 4; k++) { // determinar vizinhos adjacentes
 					int linha_viz = i + deslocamentos[k][0];
 					int coluna_viz = j + deslocamentos[k][1];
 
-					// Verificar se a posição está dentro dos limites da matriz
+					// verificar se o vizinho está dentro dos limites da matriz
 					if (linha_viz >= 0 && linha_viz < LINHAS && coluna_viz >= 0 && coluna_viz < COLUNAS) {
+						// poderiamos tirar um perrengue de matriz_despejo[i][j] para cada vizinho
+						// mas já fizemos acima e assim não precisamos considerar as janelas
+						//vizinho válido --> despeja perrengue nele
 						matriz_despejo[linha_viz][coluna_viz]++;
 					}
 				}	
